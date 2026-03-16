777bfffce4f4d88e3bf5bd9db6f0a0b552bd319f
Wasim7x <wasim7x@gmail.com>
2026-03-16 12:12:17 +0530

    code genrator module completed

---
+++ src/node/worker/coder.py
@@ -0,0 +1,53 @@
+import sys
+from src import logger
+from src import exception
+from src.state.sdlc_state import SDLCState
+class CodeGenerator:
+    def __init__(self, llm):
+        self.llm = llm  
+
+    def generate_code(self, state: SDLCState):
+            """
+                Generates the code for the requirements in the design document
+            """
+            logger.info("Generating code based on the SDLC state")
+            prompt = f"""
+            Generate Python code based on the following SDLC state:
+
+                Project Name: {state['project_name']}
+
+                ### Requirements:
+                {"".join([f"- {req}\n" for req in state['requirements']])}
+
+                ### User Stories:
+                {"".join([f"- {story['title']}: {story['description']}\n" for story in state['user_stories']])}
+
+                ### Functional Design Document:
+                {state['design_documents']['functional']}
+
+                ### Technical Design Document:
+                {state['design_documents']['technical']}
+
+                The generated Python code should include:
+
+                1. **Comments for Requirements**: Add each requirement as a comment in the generated code.
+                2. **User Stories Implementation**: Include placeholders for each user story, with its description and acceptance criteria as comments.
+                3. **Functional Design Reference**: Incorporate the functional design document content as a comment in the relevant section.
+                4. **Technical Design Reference**: Include the technical design document details in a comment under its section.
+                5. **Modularity**: Structure the code to include placeholders for different functionalities derived from the SDLC state, with clear comments indicating where each functionality should be implemented.
+                6. **Python Formatting**: The generated code should follow Python syntax and best practices.
+
+                Ensure the output code is modular, well-commented, and ready for development.
+            """
+            try:     
+                response = self.llm.invoke(prompt)
+            except Exception as e:
+                raise exception.MyException(e, sys)                
+            next_required_input = "code_review" if state['design_documents']['review_status'] == "approved" else "create_design_document"
+            code_review_comments = self.get_code_review_comments(code=response.content)
+            return {
+                    'code_generated': response.content, 
+                    'next_required_input': next_required_input, 
+                    'current_node': 'generate_code',
+                    'code_review_comments': code_review_comments
+                }
